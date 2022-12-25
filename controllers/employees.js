dbaec4a1f6228972e0e7a9752284b250482ea576
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2022-12-24 22:48:25 -0300

    feat: adds auth to employee routes

--- controllers/employees.js
+++ controllers/employees.js
@@ -120,14 +120,13 @@ exports.updateEmployee = (req, res, next) => {
 }
 
 exports.deleteEmployee = (req, res, next) => {
-    //TO-DO: check if user who's trying to delete an employee has access type 'admin'
     
-    // const accessType = req.params.accessType;
-    // if (accessType !== 'admin') {
-    //     const error = new Error('Not authorized!');
-    //     error.statusCode = 403;
-    //     throw error;
-    // }
+    const accessType = req.accessType;
+    if (accessType !== 'admin') {
+        const error = new Error('Not authorized!');
+        error.statusCode = 403;
+        throw error;
+    }
     const employeeId = req.params.employeeId;
     Employee.findByPk(employeeId)
         .then(employee => {
@@ -143,6 +142,9 @@ exports.deleteEmployee = (req, res, next) => {
             res.status(200).json({ message: 'Deleted employee.' });
         })
         .catch(err => {
-            console.log('Error deleting employee.');
+            if (!err.statusCode) {
+                err.statusCode = 500;
+              }
+              next(err);
         });
 }
--- routes/employees.js
+++ routes/employees.js
@@ -2,28 +2,29 @@ const express = require('express');
 const router = express.Router();
 const employeesController = require('../controllers/employees');
 const { body } = require('express-validator');
+const isAuth = require('../middlewares/is-auth');
 
 // GET /employees
-router.get('/employees', employeesController.getEmployees);
+router.get('/employees', isAuth, employeesController.getEmployees);
 
 // GET /employee/:employeeId
-router.get('/employee/:employeeId', employeesController.getEmployee);
+router.get('/employee/:employeeId', isAuth, employeesController.getEmployee);
 
 // POST /employee
-router.post('/employee', [
+router.post('/employee', isAuth, [
     body('fullName')
         .trim()
         .isLength({ min: 5 }),
 ], employeesController.registerEmployee);
 
 // PUT /employee/:employeeId
-router.put('/employee/:employeeId', [
+router.put('/employee/:employeeId', isAuth, [
     body('fullName')
         .trim()
         .isLength({ min: 5 }),
 ], employeesController.updateEmployee);
 
 // DELETE /employee/:employeeId
-router.delete('/employee/:employeeId', employeesController.deleteEmployee);
+router.delete('/employee/:employeeId', isAuth, employeesController.deleteEmployee);
 
 module.exports = router;
