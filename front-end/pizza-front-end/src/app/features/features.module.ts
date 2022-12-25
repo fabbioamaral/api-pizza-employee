ab26844dd88561e6c5ab7c88fa5113384908b292
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2022-12-25 00:44:06 -0300

    feat: adds homepage, fix routes, etc

---
+++ front-end/pizza-front-end/src/app/features/features.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { HomepageComponent } from "./homepage/homepage.component";
+
+@NgModule({
+    declarations: [ HomepageComponent ],
+    imports: [ ],
+    exports: []
+})
+
+export class FeaturesModule {}
---
+++ front-end/pizza-front-end/src/app/shared/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+
+import { MatCardModule } from '@angular/material/card';
+
+const materialModules = [MatCardModule];
+
+@NgModule({
+    declarations: [],
+    imports: [...materialModules],
+    exports: [...materialModules]
+})
+
+export class MaterialModule {}
