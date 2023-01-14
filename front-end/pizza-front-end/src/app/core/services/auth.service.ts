f345c840e859dded387cfc4f162a22b011c39332
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2023-01-13 23:38:12 -0300

    feat: adds logout fn

--- front-end/pizza-front-end/src/app/core/services/auth.service.ts
+++ front-end/pizza-front-end/src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginResponse } from 'src/app/features/login/model/login.model';
 
@@ -9,10 +10,21 @@ import { LoginResponse } from 'src/app/features/login/model/login.model';
 export class AuthService {
   private url = 'http://localhost:8080/auth';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) { }
 
   login(email: string, password: string): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(`${this.url}/login`, { email, password });
+  }
+
+  logout() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      localStorage.removeItem('token');
+      this.router.navigateByUrl('');
+    }
   }
 
   signup(email: string, password: string) {
