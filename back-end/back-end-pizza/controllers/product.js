dc2d0838ec3a54ac3926d3bb91314fc23ff3aea5
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2023-02-17 18:57:29 -0300

    feat: adds pizza border controller and routes

--- back-end/back-end-pizza/controllers/product.js
+++ back-end/back-end-pizza/controllers/product.js
@@ -6,6 +6,9 @@ const PizzaBorder = ProductExports.PizzaBorders;
 const PizzaFlavor = ProductExports.PizzaFlavors;
 const CategoryPizzaFlavor = ProductExports.CategoryPizzaFlavors;
 
+
+// Product Category
+
 exports.getProductCategories = (req, res, next) => {
     ProductCategory.findAll()
     .then(productCategories => {
@@ -91,4 +94,100 @@ exports.deleteProductCategory = (req, res, next) => {
             throw err;
         });
 }
+
+//  Pizza Border
     
+exports.getPizzaBorders = (req, res, next) => {
+    PizzaBorder.findAll()
+    .then(pizzaBorders => {
+        res.status(200).json({
+            message: 'Fetched pizza borders successfully.',
+            pizzaBorders
+        });
+    })
+    .catch(err => {
+        console.log('Error retrieving pizza borders');
+        console.log(err);
+    });
+}
+
+exports.addPizzaBorder = (req, res, next) => {
+    const name = req.body.name;
+    const price = req.body.price;
+    const size = req.body.size;
+
+    PizzaBorder.create({
+        name,
+        price,
+        size
+    })
+        .then(result => {
+            res.status(201).json({
+                message: `${name} pizza border has been added successfully.`,
+                employees: result
+            });
+        })
+        .catch(err => {
+            console.log('Error adding pizza border');
+            console.log(err);
+        });
+}
+
+exports.updatePizzaBorder = (req, res, next) => {
+    const pizzaBorderId = req.params.pizzaBorderId;
+
+    PizzaBorder.findByPk(pizzaBorderId)
+    .then(pizzaBorder => {
+        if (!pizzaBorder) {
+            const error = new Error('Could not find pizza border.');
+            error.statusCode = 404;
+            throw error; 
+        } else {
+            const name = req.body.name;
+            const price = req.body.price;
+            const size = req.body.size;
+            console.log('pizza border id');
+            console.log(pizzaBorderId);
+            return PizzaBorder.update(
+                {
+                    name,
+                    price,
+                    size
+                },
+                { where: {id: pizzaBorderId} }
+            );
+        }
+    }).then(result => {
+        res.status(200).json({
+            message: 'Pizza border updated successfully.'
+        });
+    })
+    .catch(err => {
+        console.log('Error trying to update a pizza border');
+        console.log(err);
+    });
+}
+
+exports.deletePizzaBorder = (req, res, next) => {
+
+    const pizzaBorderId = req.params.pizzaBorderId;
+    PizzaBorder.findByPk(pizzaBorderId)
+        .then(pizzaBorder => {
+            if (!pizzaBorder) {
+                const error = new Error('Could not find pizza border.');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            return PizzaBorder.destroy({where: { id: pizzaBorderId }});
+        })
+        .then(result => {
+            res.status(200).json({ message: 'Pizza border has been deleted successfully.' });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+              }
+            throw err;
+        });
+}
--- back-end/back-end-pizza/routes/product.js
+++ back-end/back-end-pizza/routes/product.js
@@ -4,6 +4,7 @@ const productCategoryController = require('../controllers/product');
 const { body } = require('express-validator');
 const isAuth = require('../middlewares/is-auth');
 
+/** Product Category */
 // GET /product-categories
 router.get('/product-categories', isAuth, productCategoryController.getProductCategories);
 
@@ -15,5 +16,18 @@ router.put('/product-category/:productCategoryId', isAuth, productCategoryContro
 
 // DELETE /product-category/:productCategoryId
 router.delete('/product-category/:productCategoryId', isAuth, productCategoryController.deleteProductCategory);
+
+/** Pizza Border */
+// GET /pizza-borders
+router.get('/pizza-borders', isAuth, productCategoryController.getPizzaBorders);
+
+// POST /pizza-border
+router.post('/pizza-border', isAuth, productCategoryController.addPizzaBorder);
+
+// PUT /pizza-border/:id
+router.put('/pizza-border/:pizzaBorderId', isAuth, productCategoryController.updatePizzaBorder);
+
+// DELETE /pizza-border/:id
+router.delete('/pizza-border/:pizzaBorderId', isAuth, productCategoryController.deletePizzaBorder);
 
 module.exports = router;
